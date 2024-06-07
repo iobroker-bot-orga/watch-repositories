67a3fd8fe4e42493a7838211f6a9d21988a7512e
Martin M. <mcm57@gmx.at>
2024-06-07 19:32:39 +0200

    format hh:mm

--- scripts/checkStaleRepofiles.js
+++ scripts/checkStaleRepofiles.js
@@ -7,6 +7,13 @@ const fs = require('node:fs');
 //const github = require('../lib/githubTools.js');
 const iobroker = require('@iobroker-bot-orga/iobroker-lib');
 
+function hhmmStr( min ) {
+
+    const hh = Math.floor( min/60 );
+    const mm = Math.floor( min - hh*60 );
+    return ("00" + hh).slice(-2) + ':' + ("00" + min).slice(-2);
+}
+
 async function exec() {
     const limit = 12 * 60; /* 12h max in minutes */
 
@@ -19,29 +26,29 @@ async function exec() {
     const latestTime = Date.parse(latestTimeStr);
     const latestDate = new Date(latestTime);
     const latestDiff = (nowTime-latestTime) / 1000 / 60;
-    console.log (`latestTime: ${latestDate.toString()} - ${latestDiff} minutes old`);
+    console.log (`latestTime: ${latestDate.toString()} - ${hhmmStr(latestDiff)} old`);
 
     const stableRepo = await iobroker.getStableRepoLive();
     const stableTimeStr = stableRepo._repoInfo?.repoTime;
     const stableTime = Date.parse(stableTimeStr);
     const stableDate = new Date(stableTime);
     const stableDiff = (nowTime-stableTime) / 1000 / 60;
-    console.log (`stableTime: ${stableDate.toString()} - ${stableDiff} minutes old`);
+    console.log (`stableTime: ${stableDate.toString()} - ${hhmmStr(stableDiff)} old`);
 
     let subject = '';
     let body = '';
 
     if ((latestDiff > limit) || (stableDiff > limit)) {
         subject = `[iob-bot] ERROR - Repository data outdated`;
-        body = `ioBroker repository watchjob detected the following problems:\n` +
-            `last update of LATEST repository was done at ${latestDate.toString} (${latestDiff} minutes ago)\n` +
-            `last update of STABLE repository was done at ${stableDate.toString} (${stableDiff} minutes ago)\n`;
+        body = `ioBroker repository watchjob detected the following problems:\n\n` +
+            `last update of LATEST repository was done at ${latestDate.toString} (${hhmmStr(latestDiff)} ago)  \n` +
+            `last update of STABLE repository was done at ${stableDate.toString} (${hhmmStr(stableDiff)} ago)  \n`;
         console.log (`\nERROR: repository data is stale\n`);
     } else{
         subject = `[iob-bot] OK - Repository data up to date`;
-        body = `ioBroker repository watchjob result:\n` +
-            `last update of LATEST repository was done at ${latestDate.toString} (${latestDiff} minutes ago)\n` +
-            `last update of STABLE repository was done at ${stableDate.toString} (${stableDiff} minutes ago)\n`;
+        body = `ioBroker repository watchjob result:\n\n` +
+            `last update of LATEST repository was done at ${latestDate.toString} (${hhmmStr(latestDiff)} ago)  \n` +
+            `last update of STABLE repository was done at ${stableDate.toString} (${hhmmStr(stableDiff)} ago)  \n`;
         console.log (`\nOK: everything seems to be fine.\n`);
     }
 
