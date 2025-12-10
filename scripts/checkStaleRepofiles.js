437106289c7368fb5cbb0f2f60e86d9905dc2cc2
Martin M. <mcm57@gmx.at>
2025-12-10 22:23:13 +0100

    Update checkStaleRepofiles.js

--- scripts/checkStaleRepofiles.js
+++ scripts/checkStaleRepofiles.js
@@ -71,6 +71,7 @@ async function exec() {
     });
 
     // Send Telegram notification only on error
+    isError=true;
     if (isError) {
         const botToken = process.env.TELEGRAM_BOT_TOKEN;
         const chatId = process.env.TELEGRAM_CHAT_ID;
